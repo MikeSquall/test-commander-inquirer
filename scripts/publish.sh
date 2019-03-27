0b13bea2d8b14a27dffb92a80591d6fc58c97837
mikearbib <arbib.michael@gmail.com>
2019-03-27 20:27:00 +0200

    Enabled Travis

---
+++ scripts/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
+if [[ "$TRAVIS_BRANCH" =~ ^feature\/.*$ ]]; then
+    BRANCH_NAME=$(echo $TRAVIS_BRANCH | sed "s/[/]/-/g")
+    TIMESTAMP=$(date +"%s")
+    echo "--------------------------------------------"
+    echo "|    Deploying snapshot on npm registry    |"
+    echo "--------------------------------------------"
+    npm version prerelease --preid=$BRANCH_NAME
+    npm publish --tag pull-request
+elif [[ "$TRAVIS_BRANCH" == "develop" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo "--------------------------------------------"
+    echo "|     Deploying latest on npm registry     |"
+    echo "--------------------------------------------"
+    npm version prepatch
+    npm publish --tag next
+else
+    echo "*************************************************"
+    echo "*   Not a pull request, npm publish skipped !   *"
+    echo "*************************************************"
+fi
